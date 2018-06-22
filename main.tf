5f26e97fda9cd19bf7f32649a6c35b728c70a397
Dave Arnold <darnold@hashicorp.com>
2018-06-21 17:33:10 -0700

    fuck it

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source       = "app.terraform.io/Darnold-Hashicorp/vpc/aws"
-  version      = "${var.vpc_versio}"
+  version      = "1.0.1"
   vpc_cidr     = "${var.vpc_cidr}"
   network_name = "${var.network_name}"
 }
@@ -9,7 +9,7 @@ data "aws_availability_zones" "available" {}
 
 module "public-subnet1" {
   source            = "app.terraform.io/Darnold-Hashicorp/public-subnet/aws"
-  version           = "${var.public_subnet_version}"
+  version           = "1.0.2"
   vpc_id            = "${module.vpc.vpc_id}"
   route_table_id    = "${module.vpc.route_table_id}"
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
@@ -19,7 +19,7 @@ module "public-subnet1" {
 
 module "public-subnet2" {
   source            = "app.terraform.io/Darnold-Hashicorp/public-subnet/aws"
-  version           = "${var.public_subnet_version}"
+  version           = "1.0.2"
   vpc_id            = "${module.vpc.vpc_id}"
   route_table_id    = "${module.vpc.route_table_id}"
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
@@ -29,7 +29,7 @@ module "public-subnet2" {
 
 module "private-subnet1" {
   source            = "app.terraform.io/Darnold-Hashicorp/private-subnet/aws"
-  version           = "${var.private_subnet_version}"
+  version           = "1.0.2"
   vpc_id            = "${module.vpc.vpc_id}"
   public_subnet_id  = "${module.public-subnet1.subnet_id}"
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
@@ -49,7 +49,7 @@ module "private-subnet2" {
 
 module "bastion1" {
   source           = "app.terraform.io/Darnold-Hashicorp/bastion/aws"
-  version          = "1.0.5"
+  version          = "1.0.6"
   admin_sg         = "${module.private-subnet1.admin_sg}"
   network_name     = "${var.network_name}"
   key_name         = "${var.key_name}"
