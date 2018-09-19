5e11a6291f061840375ef9e057f34f3a5f32a513
David Arnold <dja+djaboxx@sonic.net>
2018-09-19 12:20:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source       = "app.terraform.io/Darnold-Hashicorp/vpc/aws"
+  source       = "github.com/HappyPathway/terraform-aws-vpc"
   version      = "1.0.1"
   vpc_cidr     = "${var.vpc_cidr}"
   network_name = "${var.network_name}"
 }
 
 module "public-subnet" {
-  source            = "app.terraform.io/Darnold-Hashicorp/public-subnet/aws"
+  source            = "github.com/HappyPathway/terraform-aws-public-subnet"
   version           = "1.0.2"
   vpc_id            = "${module.vpc.vpc_id}"
   route_table_id    = "${module.vpc.route_table_id}"
@@ -16,7 +16,7 @@ module "public-subnet" {
 }
 
 module "private-subnet" {
-  source            = "app.terraform.io/Darnold-Hashicorp/private-subnet/aws"
+  source            = "github.com/HappyPathway/terraform-aws-private-subnet"
   version           = "1.0.2"
   vpc_id            = "${module.vpc.vpc_id}"
   public_subnet_id  = "${module.public-subnet.subnet_id}"
@@ -26,7 +26,7 @@ module "private-subnet" {
 }
 
 module "bastion" {
-  source           = "app.terraform.io/Darnold-Hashicorp/bastion/aws"
+  source           = "github.com/HappyPathway/terraform-aws-bastion"
   version          = "1.0.2"
   admin_sg         = "${module.private-subnet.admin_sg}"
   cluster_name     = "${var.network_name}"
